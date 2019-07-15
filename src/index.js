5844d909cb0d94124ca47073fd30b7c3d5ad9b0a
Aleksandr Snigirev <aleksandrsnigirev@Aleksandrs-MacBook-Pro.local>
2019-07-15 23:25:04 +0500

    connection to bootstrap and css

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom';
 import AppHeader from './components/app-header';
 import SearchPanel from './components/search-panel';
 import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
 
+import './index.css';
 
 const App = () => {
 
@@ -15,9 +17,13 @@ const App = () => {
     ];
 
     return (
-    <div>
-        <AppHeader />
+    <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
         <SearchPanel />
+        <ItemStatusFilter />
+        </div>
+
         <TodoList todos={todoData}/>
     </div>
     );
