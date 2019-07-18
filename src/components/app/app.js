afe213c1e377957a30e770536a14ae20289cb100
Aleksandr Snigirev <aleksandrsnigirev@Aleksandrs-MacBook-Pro.local>
2019-07-18 23:56:52 +0500

    important

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -81,7 +81,24 @@ export default class App extends Component {
     };
 
     onToggleImportant = (id) => {
-        console.log('Toggle Important', id);
+        this.setState(({todoData}) => {
+            const idx = todoData.findIndex((el) => el.id === id);
+
+            const oldItem = todoData[idx];
+            const newItem = {
+                ...oldItem, 
+                important: !oldItem.important};
+
+            const newArray = [
+                ...todoData.slice(0, idx),
+                newItem,
+                ...todoData.slice(idx + 1)
+            ];
+         
+            return {
+                todoData: newArray
+            };
+        });
     };
 
     render() {
